9c85682388e31d22f410472641bbbf5bf53158d6
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2024-10-23 15:43:18 +0530

    added first section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='absolute top-0 w-full h-[15vh] flex items-center justify-between md:justify-between  md:px-[5em] px-[2em] box-border'>
       <Image className='hidden md:flex' alt='logo' height={100} width={280} src={'/images/logo.png'} />
-      <Image className='flex md:hidden' alt='logo' height={50} width={130} src={'/images/logo.png'} />
+      <Image className='flex md:hidden' alt='logo' height={70} width={180} src={'/images/logo.png'} />
       <div className="w-[50%] h-full hidden md:flex items-center justify-evenly pr-[5em]">
         <a href='#' className='text-white text-[1em] hover:font-bold cursor-pointer'>Home</a>
         <a href='#' className='text-white text-[1em] hover:font-bold cursor-pointer'>Products</a>
