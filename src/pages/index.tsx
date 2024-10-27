69effc5b7d35a1c9b6af01dd45dd720bec3c4de1
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2024-10-27 15:21:40 +0530

    fixed errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,7 @@ export default function Home() {
 
           },
           (error) => {
+            console.log(error)
             setButtonTitle("failed")
             setErrorType("failed")
           },
