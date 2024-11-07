97ab6a498e3e98b5d5be72573152abbf8e5a06f8
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2024-11-07 22:50:16 +0530

    added minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,17 @@ import styles from '@/styles/Main.module.css'
 
 const Footer = () => {
   return (
-    <footer className={`${styles.footer} w-full h-[45vh] md:h-[25vh] lg:h-[35vh] bg-white flex flex-col md:flex-row items-center justify-between px-[1em] md:px-[5em] pt-4 md:pt-0 pb-5 md:pb-0`}>
+    <footer className={`${styles.footer} w-full h-[50vh] md:h-[25vh] lg:h-[35vh] bg-white flex flex-col md:flex-row items-center justify-between px-[1em] md:px-[5em] pt-4 md:pt-0 pb-5 md:pb-0`}>
         <div className="w-[30%] h-full flex flex-col md:flex-row items-center justify-start pb-3 box-border">
-          <Image src={'/images/logo-black.png'} width={90} height={80} alt="logo-black" />
-          <Image src={'/images/logotext.png'} width={170} height={90} alt="logo-black" className="ml-10 md:ml-3 mt-2 md:mt-0 " />
+          <Image src={'/images/logocolorvertcal.png'} className='flex md:hidden' width={140} height={100} alt="logo-black" />
+          <Image src={'/images/logocolorhorizontal.png'} width={230} height={130} alt="logo-black" className="hidden md:flex ml-10 md:ml-3 mt-2 md:mt-0 " />
         </div>
-        <div className="w-full md:w-[30%] h-full flex flex-row md:flex-col items-start pl-5 box-border justify-center">
-          <div className="w-1/2 h-full md:w-full md:h-1/2 flex flex-col items-start justify-start md:hidden">
+        <div className="w-full md:w-[30%] h-full flex flex-col items-center md:items-start box-border justify-start">
+          {/* <div className="w-1/2 h-full md:w-full md:h-1/2 flex flex-col items-start justify-start md:hidden">
             <span className="text-black text-[0.8em] ">Tel: 0097172687688</span>
-            <span className="text-black text-[0.8em] ">Mobile: +971 542003798</span>
-            <span className="text-black text-[0.8em] ">Mobile: +971 542003797</span>
+            <span className="text-black text-[0.8em] ">Mobile: +971 0542003799</span>
+            <span className="text-black text-[0.8em] ">WhatsApp: +971 0542003798</span>
+            <span className="text-black text-[0.8em] ">www.zeusdxb.com</span>
             <a href="mailto:sales@zeusdxb.com" className="text-black text-[0.8em]">Email: <span className="font-semibold">sales@zeusdxb.com</span></a>
             <a href="mailto:operation@zeusdxb.com" className="text-black text-[0.8em] ml-0 md:ml-5 font-semibold">operation@zeusdxb.com</a>
           </div>
@@ -21,15 +22,16 @@ const Footer = () => {
             <span className="text-black text-[0.8em] ">PO Box 10050</span>
             <span className="text-black text-[0.8em] ">Ras Al Khaimah</span>
             <span className="text-black text-[0.8em] ">Business Center 03</span>
-          </div>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>Tel: 0097172687688</span>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>Mobile: +971 542003798</span>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>Mobile: +971 542003797</span>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>PO Box 10050</span>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>Ras Al Khaimah</span>
-          <span className={`${styles.footerItem}text-black text-[1em] hidden md:flex`}>Business Center 03</span>
-          <a href="mailto:sales@zeusdxb.com" className="text-black text-[1em] hidden md:flex">Email: <span className="font-semibold">sales@zeusdxb.com</span></a>
-          <a href="mailto:operation@zeusdxb.com" className="text-black text-[1em] ml-5 font-semibold hidden md:flex">operation@zeusdxb.com</a>
+          </div> */}
+          <span className={`${styles.footerItem}text-black text-[1em] `}>Tel: 0097172687688</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>Mobile: +971 0542003799</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>WhatsApp: +971 0542003798</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>www.zeusdxb.com</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>PO Box 10050</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>Ras Al Khaimah</span>
+          <span className={`${styles.footerItem}text-black text-[1em] `}>Business Center 03</span>
+          <a href="mailto:sales@zeusdxb.com" className="text-black text-[1em] ">Email: <span className="font-semibold">sales@zeusdxb.com</span></a>
+          <a href="mailto:operation@zeusdxb.com" className="text-black text-[1em] ml-5 font-semibold ">operation@zeusdxb.com</a>
         </div>
       </footer>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,32 +5,33 @@ import styles from '@/styles/Main.module.css'
 import { useRouter } from 'next/router'
 
 
-const Header = ({ onClick,darkTheme = false }: { onClick:() => void,darkTheme?:boolean }) => {
+const Header = ({ onClick, darkTheme = false }: { onClick: () => void, darkTheme?: boolean }) => {
 
   const router = useRouter()
 
   return (
     <header className={`${styles.Header} absolute top-0 w-full h-[15vh] flex items-center justify-between md:justify-between  md:px-[5em] px-[2em] box-border`}>
       {
-        darkTheme?<Image data-aos="fade-down" className={`${styles.logo} hidden md:flex`} alt='logo' height={100} width={280} src={'/images/logoblackwithtext.png'} />:<Image data-aos="fade-down" className={`${styles.logo} hidden md:flex`} alt='logo' height={100} width={280} src={'/images/logo.png'} />
+        darkTheme ? <Image data-aos="fade-down" className={`${styles.logo} hidden md:flex`} alt='logo' height={100} width={280} src={'/images/logoblackwithtext.png'} /> : <Image data-aos="fade-down" className={`${styles.logo} hidden md:flex`} alt='logo' height={100} width={280} src={'/images/logo.png'} />
       }
       {
-        darkTheme?<Image data-aos="fade-down" className='flex md:hidden' alt='logo' height={70} width={180} src={'/images/logoblackwithtext.png'} />:<Image data-aos="fade-down" className='flex md:hidden' alt='logo' height={70} width={180} src={'/images/logo.png'} />
+        darkTheme ? <Image data-aos="fade-down" className='flex md:hidden' alt='logo' height={70} width={180} src={'/images/logoblackwithtext.png'} /> : <Image data-aos="fade-down" className='flex md:hidden' alt='logo' height={70} width={180} src={'/images/logo.png'} />
       }
       <div className="w-[50%] h-full hidden md:flex items-center justify-evenly pr-[5em]">
         <a data-aos="fade-down" onClick={() => {
           router.push("/")
-        }} href='#home' className={`${styles.headerItem} ${darkTheme?'text-black':'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Home</a>
-        <span data-aos="fade-down" onClick={() => router.push("/products")} className={`${styles.headerItem} ${darkTheme?'text-black':'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Products</span>
+        }} href='#home' className={`${styles.headerItem} ${darkTheme ? 'text-black' : 'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Home</a>
+        <span data-aos="fade-down" onClick={() => router.push("/products")} className={`${styles.headerItem} ${darkTheme ? 'text-black' : 'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Products</span>
         <a data-aos="fade-down" onClick={() => {
           router.push("/")
-        }} href='#services' className={`${styles.headerItem} ${darkTheme?'text-black':'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Services</a>
+        }} href='#aboutus' className={`${styles.headerItem} ${darkTheme ? 'text-black' : 'text-white'} text-[1em] hover:font-bold cursor-pointer`}>About</a>
         <a data-aos="fade-down" onClick={() => {
           router.push("/")
-        }} href='#aboutus' className={`${styles.headerItem} ${darkTheme?'text-black':'text-white'} text-[1em] hover:font-bold cursor-pointer`}>About</a>
+        }} href='#services' className={`${styles.headerItem} ${darkTheme ? 'text-black' : 'text-white'} text-[1em] hover:font-bold cursor-pointer`}>Services</a>
+
       </div>
       {
-        darkTheme?<Image data-aos="fade-right" onClick={onClick} className='flex md:hidden' alt='burger' height={20} width={45} src={'/images/burger-black.png'} />:<Image data-aos="fade-right" onClick={onClick} className='flex md:hidden' alt='burger' height={20} width={45} src={'/images/burger.png'} />
+        darkTheme ? <Image data-aos="fade-right" onClick={onClick} className='flex md:hidden' alt='burger' height={20} width={45} src={'/images/burger-black.png'} /> : <Image data-aos="fade-right" onClick={onClick} className='flex md:hidden' alt='burger' height={20} width={45} src={'/images/burger.png'} />
       }
 
     </header>
