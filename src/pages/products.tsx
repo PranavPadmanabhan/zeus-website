ab94245527ae362fad9f2ded18456ae228d4b70c
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2024-11-08 11:36:20 +0530

    fixed loading issue

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,24 +1,77 @@
 import Header from '@/components/Header'
 import { products } from '@/constants/constants'
 import { useAppContext } from '@/contexts/AppContext'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/styles/Main.module.css'
 import Footer from '@/components/Footer'
 import Drawer from '@/components/Drawer'
+import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Logo } from '@/components/icons'
 
+const preloadImages = (srcArray: string[]): Promise<void[]> => {
+  return Promise.all(
+    srcArray.map((src) => {
+      return new Promise<void>((resolve) => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.onload = () => resolve();
+      });
+    })
+  );
+};
 
 const Products = () => {
   const { isModalOpen, setIsModalOpen } = useAppContext()
+  const [areImagesLoaded, setAreImagesLoaded] = useState<boolean>(false);
+
+  const getImages = () => {
+    let images: string[] = []
+    products.map(prod => {
+      prod.map(item => {
+        images = [...images, item.url]
+      })
+    })
+    return images
+  }
+
+  useEffect(() => {
+    preloadImages(["/images/logoblackwithtext.png", '/images/burger-black.png', ...getImages()]).then(() => {
+      setAreImagesLoaded(true);
+    });
+    // if(typeof window !== undefined){
+    //   window.location.reload()
+    // }
+  }, [])
+
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
+  useEffect(() => {
+    console.log(areImagesLoaded)
+  }, [areImagesLoaded])
+
+
   return (
     <div className='relative w-full min-h-screen flex flex-col items-center justify-start bg-white pt-[10vh] lg:pt-[15vh] box-border'>
       <Header darkTheme onClick={() => setIsModalOpen(true)} />
+
       {
-        isModalOpen && <Drawer darkTheme/>
+        !areImagesLoaded && (
+          <div className="fixed z-[100] top-0 w-full h-[100vh] bg-black flex flex-col items-center justify-center">
+            <Logo width={150} height={100} className="animate-pulse" />
+            <span className="text-white text-[1.1em] mt-2 animate-pulse">Getting Products..</span>
+          </div>
+        )
       }
-      <h1 className={`${styles.headingProducts}text-black font-bold text-[1.8em] self-start ml-[5%] my-5`}>Products</h1>
       {
-        products.map((item, i) => (
+        isModalOpen && <Drawer darkTheme />
+      }
+      {areImagesLoaded && <h1 className={`${styles.headingProducts}text-black font-bold text-[1.8em] self-start ml-[5%] my-5`}>Products</h1>}
+      {
+        areImagesLoaded && products.map((item, i) => (
           <div key={i} className={`${styles.gridContainer} w-full min-h-[50vh] h-auto flex flex-col items-center justify-start`}>
             <div className=" w-[95%] h-[1px] bg-gray-500 flex items-center justify-start mt-2 mb-8">
               <div className="bg-white h-[40px] w-[60px] flex items-center justify-center font-bold text-[1.4em] tracking-wider text-gray-500 ml-[10%]">#{i + 1}</div>
