8f21fd04ccb9072b9c6e2235c134fce5836ea3a3
vichu-20 <ivichu20@gmail.com>
2025-06-11 14:52:29 +0530

    updated app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import AppContextProvider from "@/contexts/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContextProvider>
+     <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1GFCSFQV25"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1GFCSFQV25');
+        `}
+      </Script>
       <Component {...pageProps} />
     </AppContextProvider>
   );
 }
+
