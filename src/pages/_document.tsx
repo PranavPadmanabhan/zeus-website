9b19f7ce0ef304171b04e93dd63a31c761c2749c
vichu-20 <ivichu20@gmail.com>
2025-06-12 11:31:18 +0530

    added meta_title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Driving Efficiency with Top-Quality Machinery and Spare Parts Solutions" />
+        <title>Zeus Machineries |Best Industrial Spare Parts in UAE </title>
+        <meta name="description" content="Buy premium industrial spare parts and machinery components online at Zeus Machineries." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -16,8 +17,11 @@ export default function Document() {
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="Zeus" />
+        <meta name="apple-mobile-web-app-title" content="Zeus Machineries and Spare Parts" />
         <link rel="manifest" href="/site.webmanifest" />
+
+
+
       </Head>
       <body className="antialiased">
         <Main />
